f7b686928088e876a0fa1c077d769948378876f9
Haf <haf@protonmail.ch>
2021-07-05 09:44:35 +0200

    added i3lock

--- home/i3.nix
+++ home/i3.nix
@@ -1,13 +1,16 @@
+# TODO: This isn't really just about i3, there's also rofi and screen-locking
 { pkgs, ... }:
 {
   xsession = {
     enable = true;
     windowManager.i3 = {
       enable = true;
-      config.modifier = "Mod4";
-      config.window.titlebar = false;
-      config.terminal = "st";
-      config.menu = "rofi -show run";
+      config = {
+        modifier = "Mod4";
+        window.titlebar = false;
+        terminal = "st";
+        menu = "rofi -show run";
+      };
     };
   };
 
@@ -16,7 +19,14 @@
     theme = "dmenu";
   };
 
+  services.screen-locker = {
+    enable = true;
+    lockCmd = "${pkgs.i3lock}/bin/i3lock -n -c 000000";
+    inactiveInterval = 10;
+  };
+
   home.packages = with pkgs; [
+    i3lock
     i3status
   ];
 }
