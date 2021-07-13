c11551fbd5d1b4ff8276b6b0b6d128b0f7c7c3cb
Haf <haf@protonmail.ch>
2021-07-13 09:49:31 +0200

    progpagating nixpkgs to home-manager

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     nur.url = "github:nix-community/NUR";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager = { 
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     emacs-overlay.url = "github:nix-community/emacs-overlay";
     mydrvs.url = "github:huuff/derivations";
     secrets.url = "git+ssh://git@github.com/huuff/secrets.git";
@@ -31,7 +34,6 @@
 
         home-manager.nixosModules.home-manager
         {
-          #home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.${user} = import ./home/home.nix;
           home-manager.extraSpecialArgs = specialArgs;
