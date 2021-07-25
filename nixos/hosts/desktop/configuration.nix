655dc8cd8d8c3e51b75fbf36ecb89a8d40a418f1
Haf <haf@protonmail.ch>
2021-07-25 13:17:19 +0200

    added ntfs support to my desktop

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -9,6 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.useOSProber = true;
+  boot.supportedFilesystems = [ "ntfs" ];
   networking.hostName = "desktop";
  
   nix = {
