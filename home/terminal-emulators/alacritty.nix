c1c756a0dcef960b6c369ebb86ff2cdd91c7659c
Haf <haf@protonmail.ch>
2021-07-29 19:50:40 +0200

    auto copying primary to clipboard in alacritty

--- home/terminal-emulators/alacritty.nix
+++ home/terminal-emulators/alacritty.nix
@@ -6,6 +6,10 @@
         size = 10;
         normal.family = "FiraCode Nerd Font";
       };
+      # Motivation: I don't think I ever select stuff in a terminal
+      # with a different motivation than to copy stuff. So it makes sense
+      # that it's saved to clipboard
+      selection.save_to_clipboard = true;
     };
   };
 }
