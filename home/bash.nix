968f262d0c6efe263c5003a69e85c949f786f355
Haf <haf@protonmail.ch>
2021-11-11 00:12:58 +0100

    added part of a config for installing npm packages globally

--- home/bash.nix
+++ home/bash.nix
@@ -36,6 +36,8 @@
       shopt -s histappend                      # append to history, don't overwrite it
       # Save and reload the history after each command finishes
       export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+      export PATH=~/.npm-packages/bin:$PATH
+      export NODE_PATH=~/.npm-packages/lib/node_modules
     '';
   };
 
