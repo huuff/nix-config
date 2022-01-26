dcdaa94659be4a6c2dcb8486876a03eff09aa292
Haf <haf@protonmail.ch>
2022-01-26 11:06:50 +0100

    added vscode

--- home/home.nix
+++ home/home.nix
@@ -138,6 +138,10 @@
     autoVerticalOutput = true;
   };
 
+  programs.vscode = {
+    enable = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
