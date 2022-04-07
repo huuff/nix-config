b7ccfaf1d56bc92cfc886cbfe5d920820506daba
Haf <haf@protonmail.ch>
2022-04-07 17:35:26 +0200

    creating a link to jdk on opt

--- nixos/java.nix
+++ nixos/java.nix
@@ -2,24 +2,26 @@
 {
   environment = {
     systemPackages = with pkgs; [ 
-      #jdk11_headless
       jdk17_headless
     ];
 
     etc = with pkgs; {
-      #"jdk11".source = jdk11_headless;
       "jdk17".source = jdk17_headless;
     };
   };
 
-  system.activationScripts = {
-    # we use this at work
-    # it doesn't work, "read-only filesystem"
-    # do it manually for the time being
-    # TODO: Try with tmpfiles.d
-    createProdJDK = ''
-        # ln -s /etc/jdk11 /opt/prod_jdk || true
-        ln -s /etc/jdk17 /opt/jdk17 || true
-      '';
-  };
+  systemd.tmpfiles.rules = [
+    "L /opt/jdk17 - - - - /etc/jdk17"
+  ];
+
+  #system.activationScripts = {
+    ## we use this at work
+    ## it doesn't work, "read-only filesystem"
+    ## do it manually for the time being
+    ## TODO: Try with tmpfiles.d
+    #createProdJDK = ''
+        ## ln -s /etc/jdk11 /opt/prod_jdk || true
+        #ln -s /etc/jdk17 /opt/jdk17 || true
+      #'';
+  #};
 }
