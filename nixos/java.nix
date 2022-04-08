71dae5c5cfb68b05826a8f75c07c393ad45590a6
Haf <haf@protonmail.ch>
2022-04-08 17:44:48 +0200

    removed done todos

--- nixos/java.nix
+++ nixos/java.nix
@@ -13,15 +13,4 @@
   systemd.tmpfiles.rules = [
     "L /opt/jdk17 - - - - /etc/jdk17"
   ];
-
-  #system.activationScripts = {
-    ## we use this at work
-    ## it doesn't work, "read-only filesystem"
-    ## do it manually for the time being
-    ## TODO: Try with tmpfiles.d
-    #createProdJDK = ''
-        ## ln -s /etc/jdk11 /opt/prod_jdk || true
-        #ln -s /etc/jdk17 /opt/jdk17 || true
-      #'';
-  #};
 }
