2e3363d2e8bea190db28268aacd6a6bae877b624
Haf <haf@protonmail.ch>
2022-05-03 10:23:01 +0200

    added a file by error

--- home/virtualization-tools.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    vagrant
-    docker-compose # TODO: Get rid of it when I get rid of docker (making the switch to podman)
-    podman-compose
-  ];
-}
