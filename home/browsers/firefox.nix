5fe5f13a952da8d748f87cdb9a43b6570b2d464a
Haf <haf@protonmail.ch>
2022-07-27 11:58:47 +0200

    updated extensions

--- home/browsers/firefox.nix
+++ home/browsers/firefox.nix
@@ -12,13 +12,15 @@
 
     # TODO: Seems like this isn't getting updated? I'm missing:
     # * wallabag
-    # * vue
     # others I have at work but not here (installed manually)
     extensions = with pkgs.nur.repos.rycee.firefox-addons; [
       ublock-origin
       #leechblock-ng # TODO: It's broken
       i-dont-care-about-cookies
       decentraleyes
+      vue-js-devtools
+      languagetool
+      ublacklist
       privacy-badger
       https-everywhere
       pkgs.nur.repos.rycee.firefox-addons."1password-x-password-manager"
