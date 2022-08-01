6b3c89cac8e20d8ce3856d31e45f4ffb942e2ecd
Haf <haf@protonmail.ch>
2022-08-01 11:01:44 +0200

    more gitignores

--- home/git.nix
+++ home/git.nix
@@ -15,6 +15,7 @@
       "build" # gradle output
       ".gradle" # some gradle specifics I dunno
       "hs_err_pid*.log" # some Java crash logs
+      "replay_pid*.log" # other random Java crash logs I guess
     ];
     extraConfig = {
       credential.helper = "cache --timeout=3600";
