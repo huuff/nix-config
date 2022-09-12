e1927305bb400962f794004f8ae5720258f41757
Haf <haf@protonmail.ch>
2022-09-12 16:00:43 +0200

    added bash and zsh completions for system tools

---
+++ nixos/system-completions.nix
@@ -0,0 +1,9 @@
+{ }:
+{
+  # Apparently, completion of shells installed through home-manager (bash, zsh) needs linking of their
+  # completions dir to be able to work with system packages (such as systemctl).
+  environment.pathsToLink = [
+    "/share/bash-completion"
+    "/share/zsh"
+  ];
+}
