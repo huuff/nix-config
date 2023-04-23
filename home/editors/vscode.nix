61d757eab9039bff1885b07d11636244435ee036
Haf <haf@protonmail.ch>
2023-04-23 09:57:40 +0200

    separated vscode

---
+++ home/editors/vscode.nix
@@ -0,0 +1,58 @@
+{ pkgs, ... }:
+
+{
+  # TODO: css modules
+  # TODO: Pretty ts errors?
+  # TODO:  Maybe remove blame for gitlens?
+  programs.vscode = {
+    enable = true;
+    keybindings = [ # Some keybindings as in Intellij IDEA
+      {
+        key = "ctrl+alt+l";
+        command = "editor.action.formatDocument";
+      }
+
+      {
+        key = "ctrl+alt+s";
+        command = "workbench.action.files.saveAll";
+      }
+
+      {
+        key = "shift+f6";
+        command = "editor.action.rename";
+        when = "editorHasRenameProvider && editorTextFocus && !editorReadonly";
+      }
+
+      {
+        key = "shift shift";
+        command = "workbench.action.quickOpen";
+      }
+    ];
+    extensions = with pkgs.vscode-extensions; [
+      bradlc.vscode-tailwindcss
+      wix.vscode-import-cost
+      dbaeumer.vscode-eslint
+      pkief.material-icon-theme
+      # TODO: I don't think this is working...
+      # is it automatically picking up direnv?
+      mkhl.direnv
+      waderyan.gitblame
+      formulahendry.auto-rename-tag
+      formulahendry.auto-close-tag
+      christian-kohler.path-intellisense
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "vscode-jest";
+        publisher = "Orta";
+        version = "5.0.3";
+        sha256 = "9v2de1ETxe3A18Wq1fnYl+f65UsNPmdpqmBJ8M4p3Wc=";
+      } 
+      {
+        name = "better-comments";
+        publisher = "aaron-bond";
+        version = "3.0.2";
+        sha256 = "hQmA8PWjf2Nd60v5EAuqqD8LIEu7slrNs8luc3ePgZc=";
+      } 
+    ];
+  };
+}
--- home/home.nix
+++ home/home.nix
@@ -2,12 +2,13 @@
 {
     imports = [
       ./editors/vim/nvim.nix
+      ./editors/vscode.nix
 
       ./browsers/firefox.nix
 
       ./terminal-emulators/alacritty.nix
 
-      # TODO: Improve my modules. UPDATE: Ditch them entirely, redo them better, I'm currently using `modles` for my (better separated) modules
+      # TODO: Improve my modules. UPDATE: Ditch them entirely, redo them better, I'm currently using `modules` for my (better separated) modules
       myModules.maven
       myModules.mycli
 
@@ -157,61 +158,6 @@
     autoVerticalOutput = true;
   };
 
-  # TODO: Put it in a different file
-  # TODO: css modules
-  # TODO: Pretty ts errors?
-  # TODO:  Maybe remove blame for gitlens?
-  programs.vscode = {
-    enable = true;
-    keybindings = [ # Some keybindings as in Intellij IDEA
-      {
-        key = "ctrl+alt+l";
-        command = "editor.action.formatDocument";
-      }
-
-      {
-        key = "ctrl+alt+s";
-        command = "workbench.action.files.saveAll";
-      }
-
-      {
-        key = "shift+f6";
-        command = "editor.action.rename";
-        when = "editorHasRenameProvider && editorTextFocus && !editorReadonly";
-      }
-
-      {
-        key = "shift shift";
-        command = "workbench.action.quickOpen";
-      }
-    ];
-    extensions = with pkgs.vscode-extensions; [
-      bradlc.vscode-tailwindcss
-      wix.vscode-import-cost
-      dbaeumer.vscode-eslint
-      pkief.material-icon-theme
-      # TODO: I don't think this is working...
-      # is it automatically picking up direnv?
-      mkhl.direnv
-      waderyan.gitblame
-      formulahendry.auto-rename-tag
-      formulahendry.auto-close-tag
-      christian-kohler.path-intellisense
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "vscode-jest";
-        publisher = "Orta";
-        version = "5.0.3";
-        sha256 = "9v2de1ETxe3A18Wq1fnYl+f65UsNPmdpqmBJ8M4p3Wc=";
-      } 
-      {
-        name = "better-comments";
-        publisher = "aaron-bond";
-        version = "3.0.2";
-        sha256 = "hQmA8PWjf2Nd60v5EAuqqD8LIEu7slrNs8luc3ePgZc=";
-      } 
-    ];
-  };
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
