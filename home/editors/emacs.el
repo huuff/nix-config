38f3927a6987fdc7aa196564d64595aa695efd26
Haf <haf@protonmail.ch>
2023-12-27 06:56:56 +0100

    lil todo

--- home/editors/emacs.el
+++ home/editors/emacs.el
@@ -98,7 +98,7 @@
 )
 
 ;; consult
-;; Example configuration for Consult
+;; TODO: Install and use ripgrep (rg)
 (use-package consult
   ;; Replace bindings. Lazily loaded due by `use-package'.
   :bind (;; C-c bindings in `mode-specific-map'
