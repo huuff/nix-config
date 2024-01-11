0ced79c0733ff916bbe0460d4bbaa10972205fc2
Haf <haf@protonmail.ch>
2024-01-11 05:20:18 +0100

    removed an old comment

--- home/editors/emacs/default.nix
+++ home/editors/emacs/default.nix
@@ -25,23 +25,7 @@
           # for consult-ripgrep
           pkgs.ripgrep
         ];
-
-        # XXX: I'm using the old tree-sitter plugin because the
-        # native integration in emacs29 doesn't please me
-        # but it's old and not updated, so the nixpkgs tree-sitter-grammars are not compatible
-        # therefore, I just recompile them with an older ABI
-        override = final: prev:
-        {
-          tree-sitter-grammars = prev.tree-sitter-grammars // {
-            tree-sitter-rust = prev.tree-sitter-grammars.tree-sitter-rust.overrideAttrs (_: {
-              nativeBuildInputs = [ final.nodejs final.tree-sitter ];
-              configurePhase = ''
-                tree-sitter generate --abi 13 src/grammar.json
-              '';
-            });
-          };
-        };
       })
       )
     ];
-}
+  }
