0885701fe610db9d9041ede57f0da97d23c0c11c
Haf <haf@protonmail.ch>
2024-01-20 12:22:48 +0100

    a todo

--- home/editors/emacs/init.el
+++ home/editors/emacs/init.el
@@ -1,4 +1,5 @@
-;; TODO: When doing dependency management with rustic (such as C-c C-c a):
+;; TODO: (popwin) Keep rustic-compilation open
+;; TODO: (popwin) When doing dependency management with rustic (such as C-c C-c a):
   ;; * do not auto-select the popup buffer, there's nothing to do there
   ;; * make it go to the bottom (contrary to other rustic-compilation buffers), since there's not much useful information there
 ;; TODO: There's some error that appears when building it with nix, build with -L to find out what it is
