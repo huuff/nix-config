218c164794109d337f3794be93c79711a02a6ca5
Haf <haf@protonmail.ch>
2024-01-21 12:42:25 +0100

    a todo

--- home/editors/emacs/init.el
+++ home/editors/emacs/init.el
@@ -83,6 +83,7 @@
 (global-set-key (kbd "C-v") 'yank)
 
 ;; delay function
+;; TODO: Move to an util.el or something?
 (defun delay-fun (f &optional delay)
   "Run some function after a delay, for initializing stuff only after everything else has initialized"
   (run-with-idle-timer (or delay 1) nil f))
