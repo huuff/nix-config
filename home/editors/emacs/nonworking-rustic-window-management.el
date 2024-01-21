c674c11142fbe841bc5dfa78d678a12d86da7b3a
Haf <haf@protonmail.ch>
2024-01-21 12:47:48 +0100

    longish command explaining my situation

--- home/editors/emacs/nonworking-rustic-window-management.el
+++ home/editors/emacs/nonworking-rustic-window-management.el
@@ -1,3 +1,12 @@
+;; XXX, TODO: I was working hard on getting rustic-compilation buffers to open differently depending on whether they were
+;; for actual compilation commands like `rustic-cargo-build` or
+;; for dependency management commands like `rustic-cargo-add`.
+;; In the end, the only "solution" I found is advising all cargo
+;; dependency commands so they set some variable and then read that variable to decide how to open a buffer with `shackle.el`.
+;; I do this in shackle.el with a :custom function for deciding the target configuration, on which I reset the variable (so not all subsequent commands run as if they were a rustic dependency managenent command).
+;; This hasn't worked because apparently the :custom function for shackle is running twice! And only the last result is used.
+;; Therefore, if I unset the dependency management variable and run the same command again, the second time it's unset!
+;; The only lame idea I have is to set up a timer to unset the variable, so it stays set for enough time to work for the shackle.el :custom function
 
 (defvar haf/ran-rustic-dependency-management nil
   "Whether some command last ran a cargo dependency management command")
