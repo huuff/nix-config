ed33bdbbf9c68939d82e17e797a645db45be4b51
Haf <haf@protonmail.ch>
2024-01-22 05:44:54 +0100

    setting timer to unset ran-rustic-dependency-management just after setting it

--- home/editors/emacs/rustic-window-management.el
+++ home/editors/emacs/rustic-window-management.el
@@ -7,14 +7,12 @@
 (defun haf/advice-set-ran-rustic-dependency-management (smth)
   "Sets 'haf/ran-rustic-dependency-management', used for advising functions that run rustic dependency management"
   (setq haf/ran-rustic-dependency-management t)
+  (delay-fun (lambda () (setq haf/ran-rustic-dependency-management nil)))
 )
 
 ;; TODO: Maybe a better name that expresses that it unsets
 ;; TODO: Maybe also check the buffer name
 (defun haf/is-rustic-dependency-management (&optional buffer)
   "Check whether this is a rustic command for dependency management (add or remove dependency), and then unsets it"
-  (if haf/ran-rustic-dependency-management
-    (delay-fun (lambda () (setq haf/ran-rustic-dependency-management nil)))
-  )
   haf/ran-rustic-dependency-management
 )
