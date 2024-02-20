fb0649f28c9d6879ef0dfdb84d98acfbc61afd01
Haf <haf@protonmail.ch>
2024-02-20 07:32:50 +0100

    split defuns to a :preface

--- home/editors/emacs/tabs.el
+++ home/editors/emacs/tabs.el
@@ -61,10 +61,7 @@
   (tab-line-tab ((t (:family "Hack"))))
   (tab-line-tab-current ((t (:inherit tab-line-tab :weight bold))))
   (tab-line-tab-modified ((t (:inherit tab-line-tab :slant italic))))
-  :config
-  ;; only use special face functions for modified tabs. I removed styles for special tabs because I didn't like nor need them
-  (setq tab-line-tab-face-functions '(tab-line-tab-face-modified))
-
+  :preface
   ;; group buffers criteria
   (defun haf/tab-line-group-by-project (buffer)
     "Use the project.el name for the buffer group"
@@ -81,6 +78,70 @@
           (t (with-current-buffer buffer
                (let ((prj (project-current)))
                  (if prj (project-name prj) "other"))))))
+
+  ;; some utility functions
+  (defun haf/tab-line-tab-is-selected (tab)
+    "Returns whether a given tab is currently selected"
+    (cdr (assoc 'selected tab)))
+  (defun haf/tab-line-tab-is-group (tab)
+    "Returns whether a given tab is a group tab"
+    (cdr (assoc 'group-tab tab)))
+  
+  ;; fun to close all non-active tabs
+  (defun haf/tab-line-close-other-tabs ()
+    "Close all tabs in current group that aren't active"
+    (interactive)
+    (let ((other-tabs (cl-remove-if #'(lambda (tab) (or
+                                                     (haf/tab-line-tab-is-selected tab)
+                                                     (haf/tab-line-tab-is-group tab))) (tab-line-tabs-buffer-groups))))
+      (dolist (tab other-tabs) (funcall (cdr (assoc 'close tab))))))
+
+  ;; I skillfully copy-pasted most of this function off tab-line-tabs-buffer-groups
+  ;; TODO: Try to remove as much code from this as I can, specially window-parameter stuff I don't even know what it is
+  (defun haf/tab-line-tabs-buffer-groups ()
+    "Returns a list of buffers in the current tab group"
+    (let* ((window-parameter (window-parameter nil 'tab-line-group))
+           (group-name (tab-line-tabs-buffer-group-name (current-buffer)))
+           (group (prog1 (or window-parameter group-name "All")
+                    (when (equal window-parameter group-name)
+                      (set-window-parameter nil 'tab-line-group nil))))
+           (buffers
+            (seq-filter (lambda (b)
+                          (equal (tab-line-tabs-buffer-group-name b) group))
+                        (funcall tab-line-tabs-buffer-list-function)))
+           (sorted-buffers (if (functionp tab-line-tabs-buffer-group-sort-function)
+                               (seq-sort tab-line-tabs-buffer-group-sort-function
+                                         buffers)
+                             buffers)))
+      sorted-buffers))
+
+  (defun haf/tab-line-tab-name (buffer &optional _buffers)
+    "Appends the index of the tab to its name"
+    (let ((tab-pos (seq-position (haf/tab-line-tabs-buffer-groups) buffer)))
+      (if tab-pos
+          (format "%d %s" tab-pos (buffer-name buffer))
+        (buffer-name buffer))))
+
+  ;; TODO: Maybe the index should start at 1 since it's much more natural on the keyboard
+  (defun haf/switch-to-tab-num (count)
+    "Switch to a tab by its number in current group"
+    (interactive "p")
+    (let ((tabs (cl-remove-if #'haf/tab-line-tab-is-group (tab-line-tabs-buffer-groups))))
+      (if-let ((target-tab (nth count tabs)))
+          (switch-to-buffer (cdr (assoc 'buffer target-tab)))
+        (error (format "There is no tab at index %d" count)))))
+
+  (evil-define-command haf/tab-line-goto-tab (count)
+    :repeat nil
+    (interactive "<c>")
+    (if (not count)
+        (tab-line-switch-to-next-tab)
+      (haf/switch-to-tab-num count)))
+
+  :config
+  ;; only use special face functions for modified tabs. I removed styles for special tabs because I didn't like nor need them
+  (setq tab-line-tab-face-functions '(tab-line-tab-face-modified))
+
 
   ;; sort buffers in a group
   ;; it's pretty important because otherwise the current one is always the first
@@ -106,66 +167,9 @@
   ;; group by project.el project name
   (setq tab-line-tabs-buffer-group-function #'haf/tab-line-group-by-project)
 
-  ;; some utility functions
-  (defun haf/tab-line-tab-is-selected (tab)
-    "Returns whether a given tab is currently selected"
-    (cdr (assoc 'selected tab)))
-  (defun haf/tab-line-tab-is-group (tab)
-    "Returns whether a given tab is a group tab"
-    (cdr (assoc 'group-tab tab)))
-  
-  ;; fun to close all non-active tabs
-  (defun haf/tab-line-close-other-tabs ()
-    "Close all tabs in current group that aren't active"
-    (interactive)
-    (let ((other-tabs (cl-remove-if #'(lambda (tab) (or
-                                                     (haf/tab-line-tab-is-selected tab)
-                                                     (haf/tab-line-tab-is-group tab))) (tab-line-tabs-buffer-groups))))
-      (dolist (tab other-tabs) (funcall (cdr (assoc 'close tab))))))
-
-  ;; TODO: Try to remove as much code from this as I can, specially window-parameter stuff I don't even know what it is
-  ;; I skillfully copy-pasted most of this function off tab-line-tabs-buffer-groups
-  (defun haf/tab-line-tabs-buffer-groups ()
-    "Returns a list of buffers in the current tab group"
-    (let* ((window-parameter (window-parameter nil 'tab-line-group))
-           (group-name (tab-line-tabs-buffer-group-name (current-buffer)))
-           (group (prog1 (or window-parameter group-name "All")
-                    (when (equal window-parameter group-name)
-                      (set-window-parameter nil 'tab-line-group nil))))
-           (buffers
-            (seq-filter (lambda (b)
-                          (equal (tab-line-tabs-buffer-group-name b) group))
-                        (funcall tab-line-tabs-buffer-list-function)))
-           (sorted-buffers (if (functionp tab-line-tabs-buffer-group-sort-function)
-                               (seq-sort tab-line-tabs-buffer-group-sort-function
-                                         buffers)
-                             buffers)))
-      sorted-buffers))
-
-  (defun haf/tab-line-tab-name (buffer &optional _buffers)
-    "Appends the index of the tab to its name"
-    (let ((tab-pos (seq-position (haf/tab-line-tabs-buffer-groups) buffer)))
-      (if tab-pos
-          (format "%d %s" tab-pos (buffer-name buffer))
-        (buffer-name buffer))))
 
   (setq tab-line-tab-name-function #'haf/tab-line-tab-name)
 
-  ;; TODO: Maybe the index should start at 1 since it's much more natural on the keyboard
-  (defun haf/switch-to-tab-num (count)
-    "Switch to a tab by its number in current group"
-    (interactive "p")
-    (let ((tabs (cl-remove-if #'haf/tab-line-tab-is-group (tab-line-tabs-buffer-groups))))
-      (if-let ((target-tab (nth count tabs)))
-          (switch-to-buffer (cdr (assoc 'buffer target-tab)))
-        (error (format "There is no tab at index %d" count)))))
-
-  (evil-define-command haf/tab-line-goto-tab (count)
-    :repeat nil
-    (interactive "<c>")
-    (if (not count)
-        (tab-line-switch-to-next-tab)
-      (haf/switch-to-tab-num count)))
 
   ;; switching tabs like in vim
   :general
