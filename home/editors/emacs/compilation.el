b49e44d71abddadb232fbacaca66cd9751620ea7
Haf <haf@protonmail.ch>
2024-02-28 10:26:11 +0100

    removed useless comment

--- home/editors/emacs/compilation.el
+++ home/editors/emacs/compilation.el
@@ -21,17 +21,3 @@
         ((file-exists-p (expand-file-name "pnpm-lock.yaml" directory)) 'pnpm)
         ((file-exists-p (expand-file-name "package-lock.json" directory)) 'npm)))
 
-;; (setq my/cargo-build-error-regexp
-;;       "--> \\(.*?\\):\\([0-9]+\\):\\([0-9]+\\)"
-;;       )
-
-;; (add-to-list 'compilation-error-regexp-alist-alist
-;;              '(my/cargo-build-error-regexp 1 2 3))
-
-;; (add-to-list 'compilation-error-regexp-alist 'my/cargo-build-error-regexp)
-
-;; (setq compilation-error-regexp-alist (eval (car (get 'compilation-error-regexp-alist 'standard-value))))
-
-;; (setq compilation-error-regexp-alist
-;;       '(("--> \\(.*?\\):\\([0-9]+\\):\\([0-9]+\\)"
-;;          1 2 3)))
