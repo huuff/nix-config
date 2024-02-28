b3280092f77f2b4fd71e773aea62a48dfc375ebd
Haf <haf@protonmail.ch>
2024-02-28 10:25:33 +0100

    compilation error regexp for rust projects

--- home/editors/emacs/compilation.el
+++ home/editors/emacs/compilation.el
@@ -11,7 +11,7 @@
   (let* ((default-directory (project-root (project-current)))
          (project-type (haf/get-project-type default-directory)))
     (cl-case project-type
-      ('cargo (compile "cargo build"))
+      ('cargo (let ((compilation-error-regexp-alist '(("--> \\(.*?\\):\\([0-9]+\\):\\([0-9]+\\)" 1 2 3)))) (compile "cargo build")))
       ('pnpm (compile "pnpm check"))
       ('npm (compile "npm run build")))))
 
@@ -20,3 +20,18 @@
   (cond ((file-exists-p (expand-file-name "Cargo.toml" directory)) 'cargo)
         ((file-exists-p (expand-file-name "pnpm-lock.yaml" directory)) 'pnpm)
         ((file-exists-p (expand-file-name "package-lock.json" directory)) 'npm)))
+
+;; (setq my/cargo-build-error-regexp
+;;       "--> \\(.*?\\):\\([0-9]+\\):\\([0-9]+\\)"
+;;       )
+
+;; (add-to-list 'compilation-error-regexp-alist-alist
+;;              '(my/cargo-build-error-regexp 1 2 3))
+
+;; (add-to-list 'compilation-error-regexp-alist 'my/cargo-build-error-regexp)
+
+;; (setq compilation-error-regexp-alist (eval (car (get 'compilation-error-regexp-alist 'standard-value))))
+
+;; (setq compilation-error-regexp-alist
+;;       '(("--> \\(.*?\\):\\([0-9]+\\):\\([0-9]+\\)"
+;;          1 2 3)))
