4d470c77647738c25f69614d05d45c5817722873
Haf <haf@protonmail.ch>
2024-03-01 05:49:05 +0100

    renamed to dominating-file instead of marker-file to follow project.el's naming

--- home/editors/emacs/compilation.el
+++ home/editors/emacs/compilation.el
@@ -6,13 +6,13 @@
   "Rust compilation mode"
   (setq-local compilation-error-regexp-alist '(("--> \\(.*?\\):\\([0-9]+\\):\\([0-9]+\\)" 1 2 3))))
 
-(setq haf/compilation-configs (list '(:marker-file "Cargo.toml" :build-command "cargo build" :mode rust-compilation-mode)
-                                    '(:marker-file "pnpm-lock.yaml" :build-command "pnpm check")
-                                    '(:marker-file "package-lock.json" :build-command "npm run build")))
+(setq haf/compilation-configs (list '(:dominating-file "Cargo.toml" :build-command "cargo build" :mode rust-compilation-mode)
+                                    '(:dominating-file "pnpm-lock.yaml" :build-command "pnpm check")
+                                    '(:dominating-file "package-lock.json" :build-command "npm run build")))
 
 (defun haf/compilation-config-applies (cfg directory)
   "Checks whether a given compilation config applies to the given directory"
-  (file-exists-p (expand-file-name (plist-get cfg :marker-file) directory)))
+  (file-exists-p (expand-file-name (plist-get cfg :dominating-file) directory)))
 
 (defun haf/find-compilation-config ()
   (let ((default-directory (project-root (project-current))))
