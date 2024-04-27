c6cb1828a2f00200bd45de0a4de42040f04c202c
Haf <haf@protonmail.ch>
2024-04-27 09:08:47 +0200

    a todo

--- home/editors/emacs/init.el
+++ home/editors/emacs/init.el
@@ -53,6 +53,7 @@
 ;; TODO: Maybe use consult-web?
 ;; TODO: ignore anything under .gitignore for consult-todo
 ;; TODO: I could remove all of my :if to check whether evil keybindings are enable and just use packages :after evil
+;; TODO: Do something cool with next-error-follow-minor-mode. For example, auto enable it in compilation mode
 ;; TODO: Maybe use wgrep?
 
 ;; refresh open buffers when filesystem changes
@@ -1240,6 +1241,7 @@ targets."
   :bind (:map markdown-mode-map
               ("C-c C-e" . markdown-do)))
 
+;; TODO: Maybe try to set up elfeed-summary as a sidebar on an activity?
 ;; elfeed
 ;; =====================
 ;; RSS reader in emacs
