ae3dcd07784c098bb8b2d0a4e727c17a271eda4b
Haf <haf@protonmail.ch>
2024-09-06 13:20:33 +0200

    switch to idea ultimate

--- home/development.nix
+++ home/development.nix
@@ -1,8 +1,9 @@
 { pkgs, ... }:
 {
+  # TODO maybe these should be tied to the specific editors/repos?
   home.packages = with pkgs; [
     nodePackages.diagnostic-languageserver
-    jetbrains.idea-ultimate
+    jetbrains.idea-community
 
     # Nix
     nixpkgs-fmt
