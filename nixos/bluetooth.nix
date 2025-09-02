e7a7d626c195849a8584e25fb9fe86effb0d53c0
Haf <haf@protonmail.ch>
2025-09-03 00:06:45 +0200

    fix: removed blueman because it was driving me insane

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,5 +1,8 @@
 { ... }:
 {
   hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
+  # TODO: disabled because once it's enabled, it seems to try to force me accept pairing
+  # through a desktop notification, which I think it's supposed to have a button for me to click
+  # and accept... only the button isn't showing for me!! Maybe it's the fault of dunst?
+  #services.blueman.enable = true;
 }
