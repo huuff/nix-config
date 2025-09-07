31ecfedfbeb26e2664a5b7cb8dfdc83f5079cd12
Haf <haf@protonmail.ch>
2025-09-07 19:22:17 +0200

    feat: nix plugin for vscode

--- home/editors/vscode.nix
+++ home/editors/vscode.nix
@@ -42,6 +42,8 @@
         rust-lang.rust-analyzer # rust-analyzer
         tamasfe.even-better-toml # TOML support
         aaron-bond.better-comments # nicer display for todos and such
+        # TODO: I think I'm missing LSP config and others
+        jnoortheen.nix-ide # nix support
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         # XXX: I'm leaving this one as an example in case I need to manually add some, but this one's already in
         # nixpkgs
