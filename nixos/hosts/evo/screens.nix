6270a7f3ef586ffd72034072a515dee5dc3df789
Haf <haf@protonmail.ch>
2025-10-02 22:49:43 +0200

    Revert "fix: 1080p on laptop screen instead of scaling"
    This reverts commit 50adb627bcc6ee71b277f8d170e15bbe794e91d0.

--- nixos/hosts/evo/screens.nix
+++ nixos/hosts/evo/screens.nix
@@ -11,10 +11,12 @@
       laptopScreenConfig = {
         enable = true;
         primary = true;
-        # TODO: this screen is high dpi (2880x1800) but scaling looks like ass so
-        # I downgrade it... maybe I need wayland for this?
-        mode = "1920x1200";
+        mode = "2880x1800";
         position = "0x0";
+        scale = {
+          x = 0.5;
+          y = 0.5;
+        };
       };
     in
     {
@@ -37,7 +39,7 @@
           "DP-1" = {
             enable = true;
             mode = "1920x1080";
-            position = "1920x0";
+            position = "2880x0";
           };
         };
       };
@@ -52,7 +54,7 @@
           "HDMI-1" = {
             enable = true;
             mode = "1920x1080";
-            position = "1920x0";
+            position = "2880x0";
           };
         };
       };
