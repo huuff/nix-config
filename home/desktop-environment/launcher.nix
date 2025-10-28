a7d1f2e2ec0d3a7a0a22e12a71d29d482bbd3846
Haf <haf@protonmail.ch>
2025-10-28 10:58:42 +0100

    style(launcher): stylix-compatible rofi theme

--- home/desktop-environment/launcher.nix
+++ home/desktop-environment/launcher.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, config, ... }:
 {
 
   programs.rofi = {
@@ -7,5 +7,69 @@
     extraConfig = {
       show-icons = true;
     };
+
+    # Rounded theme adapted from Newman Sanchez (https://github.com/newmanls)
+    # Original: https://github.com/newmanls/rofi-themes-collection
+    # Modified to use stylix colors
+    theme =
+      let
+        inherit (config.lib.formats.rasi) mkLiteral;
+      in lib.mkAfter {
+        "*" = {
+          margin = mkLiteral "0px";
+          padding = mkLiteral "0px";
+          spacing = mkLiteral "0px";
+        };
+
+        window = {
+          location = mkLiteral "north";
+          y-offset = mkLiteral "calc(50% - 176px)";
+          width = mkLiteral "480";
+          border-radius = mkLiteral "24px";
+        };
+
+        mainbox = {
+          padding = mkLiteral "12px";
+        };
+
+        inputbar = {
+          border = mkLiteral "2px";
+          border-radius = mkLiteral "16px";
+          padding = mkLiteral "8px 16px";
+          spacing = mkLiteral "8px";
+          children = map mkLiteral [ "prompt" "entry" ];
+        };
+
+        entry = {
+          placeholder = "Search";
+        };
+
+        message = {
+          margin = mkLiteral "12px 0 0";
+          border-radius = mkLiteral "16px";
+        };
+
+        textbox = {
+          padding = mkLiteral "8px 24px";
+        };
+
+        listview = {
+          margin = mkLiteral "12px 0 0";
+          lines = 8;
+          columns = 1;
+          fixed-height = false;
+        };
+
+        element = {
+          padding = mkLiteral "8px 16px";
+          spacing = mkLiteral "8px";
+          border-radius = mkLiteral "16px";
+        };
+
+        element-icon = {
+          size = mkLiteral "1em";
+          vertical-align = mkLiteral "0.5";
+        };
+      };
   };
 }
