c6066289632e78ccc9ec3861a93e4976c123b286
Haf <haf@protonmail.ch>
2025-11-02 10:24:40 +0100

    feat(launcher): search with ddg instead of google

--- home/desktop-environment/launcher.nix
+++ home/desktop-environment/launcher.nix
@@ -4,4 +4,14 @@
     enable = true;
     runAsService = true;
   };
+
+  # TODO: the provided home-manager module for configuring elephant is outdated
+  # and its configuration won't work, this is the hard way of doing it, until it gets updated
+  xdg.configFile."elephant/websearch.toml".text = ''
+    [[entries]]
+    name = "DuckDuckGo"
+    prefix = "@dd "
+    url = "https://duckduckgo.com?q=%TERM%"
+    default = true
+  '';
 }
