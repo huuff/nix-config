0c0ba4c0b810a62777087a66909f60449829fb10
Haf <haf@protonmail.ch>
2025-11-16 15:57:02 +0100

    refactor: undo my last change, didn't work at all

--- nixos/hosts/evo/configuration.nix
+++ nixos/hosts/evo/configuration.nix
@@ -22,10 +22,6 @@
     # and are totally useless
     consoleLogLevel = 3;
 
-    kernelParams = [
-      "usbcore.autosuspend=-1" # otherwise my mouse disconnects when used through a docking station
-    ];
-
     extraModulePackages = [];
   };
 
