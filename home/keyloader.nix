10ecde2792953dc4e1b4924df42e53de2f2a7c4e
Haf <haf@protonmail.ch>
2026-07-21 22:06:07 +0200

    chore: gpg preset passphrase

--- home/keyloader.nix
+++ home/keyloader.nix
@@ -4,4 +4,11 @@
   nixpkgs.overlays = [ vibe-keyloader.overlays.default ];
 
   programs.keyloader.enable = true;
+
+  # keyloader needs allow-preset-passphrase to preset key passphrases into gpg-agent
+  services.gpg-agent = {
+    enable = true;
+    extraConfig = "allow-preset-passphrase";
+    maxCacheTtl = 86400; # preset entries expire with this TTL
+  };
 }
