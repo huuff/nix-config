d278cfec76c2d24e1b9ef4392405725cfe939714
Haf <haf@protonmail.ch>
2026-09-03 21:38:12 +0200

    chore: updated

--- pkgs/playwright-cli.nix
+++ pkgs/playwright-cli.nix
@@ -1,8 +1,8 @@
 { buildNpmPackage, src }:
 buildNpmPackage {
   pname = "playwright-cli";
-  version = "0.1.1";
+  version = "0.1.19";
   inherit src;
-  npmDepsHash = "sha256-ZrO8yIqMYMQUlsQraejVgKRZ7klC5/8UsV3/H1EqYtA=";
+  npmDepsHash = "sha256-aY3i+sc2p8iQAEpfs+j/ifeBVmMpDDmwctEqOIDmCqI=";
   dontNpmBuild = true;
 }
